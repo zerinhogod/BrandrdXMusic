635cdc201763c7f91ef65a74a9f31199aea1dbab
zerinhogod <luceenavictor@gmail.com>
2024-08-13 17:52:13 -0300

    Update inline.py

--- BrandrdXMusic/cplugin/utils/inline.py
+++ BrandrdXMusic/cplugin/utils/inline.py
@@ -3,10 +3,10 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 buttons = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="▷", callback_data="resume_cb"),
-            InlineKeyboardButton(text="II", callback_data="pause_cb"),
-            InlineKeyboardButton(text="‣‣I", callback_data="skip_cb"),
-            InlineKeyboardButton(text="▢", callback_data="end_cb"),
+            InlineKeyboardButton(text="▶️", callback_data="resume_cb"),
+            InlineKeyboardButton(text="⏸️", callback_data="pause_cb"),
+            InlineKeyboardButton(text="⏩️", callback_data="skip_cb"),
+            InlineKeyboardButton(text="⏹️", callback_data="end_cb"),
         ]
     ]
 )
