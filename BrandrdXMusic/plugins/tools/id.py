fdb5fc4d95dbf398bef980af444a004cf03a0ce8
zerinhogod <luceenavictor@gmail.com>
2024-08-13 19:57:04 -0300

    Update id.py

--- BrandrdXMusic/plugins/tools/id.py
+++ BrandrdXMusic/plugins/tools/id.py
@@ -6,16 +6,16 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 def ids(_, message):
     reply = message.reply_to_message
     if reply:
-        button = InlineKeyboardButton("✯ ᴄʟᴏsᴇ ✯", callback_data="close")
+        button = InlineKeyboardButton("FECHAR", callback_data="close")
         markup = InlineKeyboardMarkup([[button]])
         message.reply_text(
-            f"User {reply.from_user.first_name} ID is : {reply.from_user.id}",
+            f"ID desse otário aqui {reply.from_user.first_name} é: {reply.from_user.id}",
             reply_markup=markup
         )
     else:
-        button = InlineKeyboardButton("✯ ᴄʟᴏsᴇ ✯", callback_data="close")
+        button = InlineKeyboardButton("FECHAR", callback_data="close")
         markup = InlineKeyboardMarkup([[button]])
         message.reply(
-           f"ᴛʜɪs ɢʀᴏᴜᴩ's ɪᴅ ɪs: {message.chat.id}",
+           f"O ID desse grupo é: {message.chat.id}",
            reply_markup=markup
         )
