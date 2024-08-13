b6d2c020c8f2cf116b1252c303ead2496f89e374
zerinhogod <luceenavictor@gmail.com>
2024-08-13 19:44:43 -0300

    Update bot_left.py

--- BrandrdXMusic/plugins/tools/bot_left.py
+++ BrandrdXMusic/plugins/tools/bot_left.py
@@ -31,14 +31,14 @@ async def on_left_chat_member(_, message: Message):
         left_chat_member = message.left_chat_member
         if left_chat_member and left_chat_member.id == (await app.get_me()).id:
             remove_by = (
-                message.from_user.mention if message.from_user else "𝐔ɴᴋɴᴏᴡɴ 𝐔sᴇʀ"
+                message.from_user.mention if message.from_user else "Usuário desconhecido"
             )
             title = message.chat.title
             username = (
-                f"@{message.chat.username}" if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐂ʜᴀᴛ"
+                f"@{message.chat.username}" if message.chat.username else "Grupo privado"
             )
             chat_id = message.chat.id
-            left = f"✫ <b><u>#𝐋ᴇғᴛ_𝐆ʀᴏᴜᴘ</u></b> ✫\n\n𝐂ʜᴀᴛ 𝐓ɪᴛʟᴇ : {title}\n\n𝐂ʜᴀᴛ 𝐈ᴅ : {chat_id}\n\n𝐑ᴇᴍᴏᴠᴇᴅ 𝐁ʏ : {remove_by}\n\n𝐁ᴏᴛ : @{app.username}"
+            left = f"<b><u>#𝐋ᴇғᴛ_𝐆ʀᴏᴜᴘ</u></b> \n\nGrupo: {title}\n\nID do group: {chat_id}\n\nRemovido por: {remove_by}\n\nBOT: @{app.username}"
             await app.send_photo(LOG_GROUP_ID, photo=random.choice(photo), caption=left)
             await delete_served_chat(chat_id)
             await userbot.leave_chat(chat_id)
