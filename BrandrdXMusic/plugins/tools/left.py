679ebff6898ef38e09cd2cf1b41646382fc0ae9d
zerinhogod <luceenavictor@gmail.com>
2024-08-13 19:59:12 -0300

    Update left.py

--- BrandrdXMusic/plugins/tools/left.py
+++ BrandrdXMusic/plugins/tools/left.py
@@ -92,8 +92,8 @@ async def member_has_left(client: app, member: ChatMemberUpdated):
                 profile_path=photo,
             )
         
-            caption = f"**#New_Member_Left**\n\n**๏** {user.mention} **ʜᴀs ʟᴇғᴛ ᴛʜɪs ɢʀᴏᴜᴘ**\n**๏ sᴇᴇ ʏᴏᴜ sᴏᴏɴ ᴀɢᴀɪɴ..!**"
-            button_text = "๏ ᴠɪᴇᴡ ᴜsᴇʀ ๏"
+            caption = f"**#New_Member_Left**\n\n{user.mention} **saiu do grupo...**\n**Aleluia!**"
+            button_text = "VER QUEM ERA"
 
             # Generate a deep link to open the user's profile
             deep_link = f"tg://openmessage?user_id={user.id}"
