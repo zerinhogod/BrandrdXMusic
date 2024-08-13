7fe0595bab96014168b6d47eeb01d1b99b2580aa
zerinhogod <luceenavictor@gmail.com>
2024-08-13 18:44:31 -0300

    Update help.py

--- BrandrdXMusic/utils/inline/help.py
+++ BrandrdXMusic/utils/inline/help.py
@@ -63,7 +63,7 @@ def first_page(_):
 
 def second_page(_):
     controll_button = [
-        InlineKeyboardButton(text="RETORNAR", callback_data=f"settings_back_helper")
+        InlineKeyboardButton(text="VOLTAR", callback_data=f"settings_back_helper")
     ]
     second_page_menu = InlineKeyboardMarkup(
         [
