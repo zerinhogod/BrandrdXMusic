76e7a3e00022b00e00bfdc949434d747318bf9f8
zerinhogod <luceenavictor@gmail.com>
2024-08-13 19:05:40 -0300

    Update settings.py

--- BrandrdXMusic/utils/inline/settings.py
+++ BrandrdXMusic/utils/inline/settings.py
@@ -25,7 +25,7 @@ def setting_markup(_):
 def vote_mode_markup(_, current, mode: Union[bool, str] = None):
     buttons = [
         [
-            InlineKeyboardButton(text="Vᴏᴛɪɴɢ ᴍᴏᴅᴇ ➜", callback_data="VOTEANSWER"),
+            InlineKeyboardButton(text="Modo de votação:", callback_data="VOTEANSWER"),
             InlineKeyboardButton(
                 text=_["ST_B_5"] if mode == True else _["ST_B_6"],
                 callback_data="VOMODECHANGE",
@@ -34,7 +34,7 @@ def vote_mode_markup(_, current, mode: Union[bool, str] = None):
         [
             InlineKeyboardButton(text="-2", callback_data="FERRARIUDTI M"),
             InlineKeyboardButton(
-                text=f"ᴄᴜʀʀᴇɴᴛ : {current}",
+                text=f"Atual: {current}",
                 callback_data="ANSWERVOMODE",
             ),
             InlineKeyboardButton(text="+2", callback_data="FERRARIUDTI A"),
