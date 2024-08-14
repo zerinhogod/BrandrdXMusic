45003d04e270d3ad60258cfcae4c2fac5f688ded
zerinhogod <luceenavictor@gmail.com>
2024-08-14 03:00:19 -0300

    Update bot_left.py

--- BrandrdXMusic/plugins/tools/bot_left.py
+++ BrandrdXMusic/plugins/tools/bot_left.py
@@ -15,11 +15,11 @@ from BrandrdXMusic.utils.database import get_assistant
 from BrandrdXMusic.utils.database import delete_served_chat
 
 photo = [
-    "https://graph.org/file/872dc8af2a36bed43b9b6.jpg",
-    "https://graph.org/file/f4b34351a59061ba1c61b.jpg",
-    "https://graph.org/file/3fb3f4c8a1250c6a50af1.jpg",
-    "https://graph.org/file/eabab7e8a3e5df87a0b04.jpg",
-    "https://graph.org/file/427f4869a158126957747.jpg",
+    "FOTO AQUI",
+    "FOTO AQUI",
+    "FOTO AQUI",
+    "FOTO AQUI",
+    "FOTO AQUI",
 ]
 
 
