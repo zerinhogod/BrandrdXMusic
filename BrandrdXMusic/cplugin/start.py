5b376d7e23918df8cb202055e423b54d038eaf46
zerinhogod <luceenavictor@gmail.com>
2024-08-14 02:56:36 -0300

    Update start.py

--- BrandrdXMusic/cplugin/start.py
+++ BrandrdXMusic/cplugin/start.py
@@ -40,9 +40,9 @@ SPAM_WINDOW_SECONDS = 5
 
 
 YUMI_PICS = [
-    "https://graph.org/file/f076ff205c6ce6c554b25.jpg",
-    "https://graph.org/file/de80d107a62ce5c987ce8.jpg",
-    "https://graph.org/file/fc1caf40a0be6d3560e3a.jpg",
+    "FOTO AQUI",
+    "FOTO AQUI",
+    "FOTO AQUI",
 ]
 
 
