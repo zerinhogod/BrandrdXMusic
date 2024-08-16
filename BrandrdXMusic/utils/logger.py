8dd735ca909b8d8047faee9e991fc49fb4ad0b0a
zerinhogod <luceenavictor@gmail.com>
2024-08-16 03:45:38 -0300

    Update logger.py

--- BrandrdXMusic/utils/logger.py
+++ BrandrdXMusic/utils/logger.py
@@ -8,18 +8,20 @@ from config import LOGGER_ID
 async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
-<b>{app.mention} log de músicas</b>
+<b>{app.mention} iniciou uma reprodução...</b>
 
+<b>Onde?</b>
 <b>ID do grupo:</b> <code>{message.chat.id}</code>
 <b>Nome do grupo:</b> {message.chat.title}
-<b>Usuário do grupo:</b> @{message.chat.username}
+<b>Acesso ao grupo:</b> @{message.chat.username}
 
+<b>Quem?</b>
 <b>ID do usuário:</b> <code>{message.from_user.id}</code>
 <b>Nome:</b> {message.from_user.mention}
 <b>Username:</b> @{message.from_user.username}
 
 <b>Consulta:</b> {message.text.split(None, 1)[1]}
-<b>Tipo de stream:</b> {streamtype}"""
+<b>Stream:</b> {streamtype}"""
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
