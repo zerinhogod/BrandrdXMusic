4d0366e6c7ae1fd5bd96beadfbe2dd20610c07ca
zerinhogod <luceenavictor@gmail.com>
2024-08-17 02:39:31 -0300

    Update all_tag.py

--- BrandrdXMusic/cplugin/all_tag.py
+++ BrandrdXMusic/cplugin/all_tag.py
@@ -51,11 +51,11 @@ async def tag_all_users(client, _, message):
             if message.chat.id not in SPAM_CHATS:
                 break
             usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id}), "
             if usernum == 5:
                 await client.send_message(
                     message.chat.id,
-                    f"{text}\n{usertxt}\n\n|| ➥ Para parar de marcar a galera é só usar: /cancel ||",
+                    f"{text}\n{usertxt}\n➥ Para parar de marcar a galera é só usar: /cancel",
                 )
                 await asyncio.sleep(2)
                 usernum = 0
