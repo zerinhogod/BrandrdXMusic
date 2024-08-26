5dcb08640c3aae37b57fcd5b67d35741a3a43e8c
zerinhogod <luceenavictor@gmail.com>
2024-08-25 23:11:15 -0300

    Update vctools.py

--- BrandrdXMusic/plugins/tools/vctools.py
+++ BrandrdXMusic/plugins/tools/vctools.py
@@ -31,19 +31,28 @@ async def brah2(_, msg):
 # invite members on vc
 @app.on_message(filters.video_chat_members_invited)
 async def brah3(app: app, message: Message):
-    text = f"{message.from_user.mention} convidou para a call: "
+    text = f"{message.from_user.mention} convidou "
     x = 0
     for user in message.video_chat_members_invited.users:
         try:
-            text += f"[{user.first_name}](tg://user?id={user.id}) "
+            text += f"[{user.first_name}](tg://user?id={user.id}) para a call"
             x += 1
         except Exception:
             pass
 
     try:
         invite_link = await app.export_chat_invite_link(message.chat.id)
+        add_link = f"https://t.me/{app.username}?startgroup=true"
         reply_text = f"{text}"
 
+        await message.reply(
+            reply_text,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton(text="ENTRAR NA CALL", url=add_link)],
+                ]
+            ),
+        )
     except Exception as e:
         print(f"Error: {e}")
 
