476366385da2f88779b93975ca22a7ede492fcd8
zerinhogod <luceenavictor@gmail.com>
2024-10-04 02:06:03 -0300

    Update sudoers.py

--- BrandrdXMusic/plugins/sudo/sudoers.py
+++ BrandrdXMusic/plugins/sudo/sudoers.py
@@ -48,7 +48,7 @@ async def userdel(client, message: Message, _):
 @language
 async def sudoers_list(client, message: Message, _):
     if message.from_user.id not in SUDOERS:
-        return await message.reply_text("<b>Donos:</b>\n1➤ <a href='https://t.me/combinadomusic'>COMBINADO</a>",
+        return await message.reply_text("<b>Donos:</b>\n1➤ <a href='https://t.me/COMBINADOASSISTANT_bot'>COMBINADO</a>",
         disable_web_page_preview=True,
         parse_mode="html")
     text = _["sudo_5"]
